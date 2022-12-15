57495d218ea4df742894ce46bbab6ce2105ea72b
Roy Straub <roy@codecraftr.nl>
2022-12-15 17:53:59 +0100

    Ignore the most complicated example

--- src/test/scala/nl/codecraftr/scala/aoc2022/dayone/CalorieCountingTest.scala
+++ src/test/scala/nl/codecraftr/scala/aoc2022/dayone/CalorieCountingTest.scala
@@ -4,15 +4,32 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class CalorieCountingTest extends AnyFlatSpec with Matchers {
-    "maxCaloriesCarriedBySingleElf" should "return the most calories a single elf carries" in {
-
-    }
-
-    it should "return 0 given no food is carried" in {
+    "maxCaloriesCarriedBySingleElf" should "return 0 given no food is carried" in {
         CalorieCounter.maxCaloriesCarriedBySingleElf("") shouldBe 0
     }
 
     it should "return the calories given a single elf carrying one piece of food" in {
         CalorieCounter.maxCaloriesCarriedBySingleElf("1000") shouldBe 1000
+    }
+
+    // Final sample case
+    ignore should "return the most calories a single elf carries" in {
+        CalorieCounter.maxCaloriesCarriedBySingleElf(
+            """
+              |1000
+              |2000
+              |3000
+              |
+              |4000
+              |
+              |5000
+              |6000
+              |
+              |7000
+              |8000
+              |9000
+              |
+              |10000
+              |""".stripMargin) shouldBe 24000
     }
 }
